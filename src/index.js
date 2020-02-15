7cb40381c4c6a63ea0973a830a93a97353d0c17a
paulloclara <paulloclara@gmail.com>
2020-02-15 13:42:00 -0300

    :rocket: Check environment variables

--- src/index.js
+++ src/index.js
@@ -7,10 +7,24 @@ const typeDefs = require("./schemas/graphql");
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
+function checkEnvVars() {
+  if (
+    process.env.INTHEGRA_URL &&
+    process.env.API_KEY &&
+    process.env.EMAIL &&
+    process.env.PASSWORD
+  )
+    return true;
+
+  console.log("\n\tConfigure the .env file!\n\tExample in .env_example\n");
+
+  return false;
+}
+
 async function run() {
   const { url } = await server.listen();
 
   console.log(`\n\tRunning on ${url}\n`);
 }
 
-run();
+if (checkEnvVars()) run();
