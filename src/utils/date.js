cf4882fe7481b22069a01c081304b024b784d3f0
paulloclara <paulloclara@gmail.com>
2020-02-15 11:10:59 -0300

    :rocket: Add current GMT date and time plus minutes and check the validity of the date

--- src/utils/date.js
+++ src/utils/date.js
@@ -7,5 +7,18 @@ module.exports = {
         .add(3, "h")
         .format("ddd, DD MMM YYYY HH:mm:ss") + " GMT"
     );
+  },
+
+  getCurrentGMTDateTimeMore(minutes) {
+    return (
+      moment()
+        .add(3, "h")
+        .add(minutes, "m")
+        .format("ddd, DD MMM YYYY HH:mm:ss") + " GMT"
+    );
+  },
+
+  checkValidity(date) {
+    return moment(this.getCurrentGMTDateTime()).isAfter(date);
   }
 };
